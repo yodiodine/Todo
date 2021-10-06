6cf0af950ed865592035c0ceab03da91d715a711
yodiodine <76445841+yodiodine@users.noreply.github.com>
2021-10-06 17:50:37 +0900

    update js
    로컬 스토리지 저장 값 수정 (json형태로), 추가 할 일 주석 체크!

--- script.js
+++ script.js
@@ -20,17 +20,22 @@ window.onload=function(){
     function clearOrNotTodo(){
         check_btns.forEach((btn)=>{
             btn.addEventListener("click",function(){
+                //클릭한 목록이 있는 배열 인덱스를 찾은 후 clear값 변경. 이후 배열 값에 따라 로컬 값 재설정
+                let idx;
+                for(let i=0; i<toDoArray.length;i++){
+                    if(toDoArray[i].todo === this.parentElement.textContent){
+                        idx = i;
+                    }
+                }
+                toDoArray[idx].clear ? toDoArray[idx].clear = false : toDoArray[idx].clear = true;
+                localStorage.setItem(idx, JSON.stringify(toDoArray[idx]));
+
                 this.parentElement.classList.contains("clear") ? 
                     this.parentElement.classList.remove("clear") :
                     this.parentElement.classList.add("clear");
     
                 cleard_list = document.querySelectorAll(".clear");
                 isSun ? brightCheck() : darkCheck();
-
-                //배열 내 해당 객체 clear값 변경 해야함 false<->true
-                //로컬로 값 보낼 때 애초에 clear값도 함께 보내었어야 함.....{todo:"something",claer" false} 이런식으로
-
-
 
             })
         })
@@ -122,15 +127,25 @@ window.onload=function(){
 
     //localStorage조회하여 배열에 넣고 화면에 보이기
     for(let i=0;i<localStorage.length;i++){
-        toDoArray.push({todo : localStorage.getItem(i),claer : false});
+        toDoArray.push({todo : JSON.parse(localStorage.getItem(i)).todo ,clear :  JSON.parse(localStorage.getItem(i)).clear});
 
-        list.innerHTML+=`<li><span class='check'><i class='fas fa-check'></i></span>${localStorage.getItem(i)}<span class='delete'><i class='far fa-trash-alt'></i></span></li>`;
+        list.innerHTML+=`<li><span class='check'><i class='fas fa-check'></i></span>${JSON.parse(localStorage.getItem(i)).todo}<span class='delete'><i class='far fa-trash-alt'></i></span></li>`;
 
         check_btns = document.querySelectorAll(".check");
         delete_btns = document.querySelectorAll(".delete");
         clearOrNotTodo();
         deleteTodo();
     }
+
+    //완료한 할 일 체크박스
+    //배열마다 순회한다 -> 클리어가 true인 배열의 인덱스를 찾아 해당 li의 클래스에 clear를 붙인다.
+    toDoArray.forEach((val,idx)=>{
+        if(val.clear){
+            list.children[idx].setAttribute("class","clear");
+            cleard_list = document.querySelectorAll(".clear");
+            isSun ? brightCheck() : darkCheck();
+        }
+    })
 
     //========================입력창에 할 일 입력========================//
     //할 일 추가 함수(+버튼 혹은 enter 키(keyCode : 13))
@@ -158,7 +173,7 @@ window.onload=function(){
         //배열에 todo push
         toDoArray.push({todo : txt , clear : false});
         //로컬에 저장
-        localStorage.setItem(toDoArray.length-1, txt);
+        localStorage.setItem(toDoArray.length-1, JSON.stringify(toDoArray[toDoArray.length-1]));
         //할 일을 목록에 표현하기 : li 추가
         list.innerHTML+=`<li><span class='check'><i class='fas fa-check'></i></span>${txt}<span class='delete'><i class='far fa-trash-alt'></i></span></li>`;
         
@@ -166,7 +181,15 @@ window.onload=function(){
         delete_btns = document.querySelectorAll(".delete");
         clearOrNotTodo();
         deleteTodo();
+        cleard_list = document.querySelectorAll(".clear");
         isSun ? brightCheck() : darkCheck();
     }
-    
 }
+
+
+
+//value 추가 후 입력 되어있던 인풋 값 지우기
+//인풋 내 텍스트 내용 바꾸기(예시 내용을 넣거나 ex: 운동 30분 하기)
+//placeholder가 새로고침하거나 등등 바뀜(더미텍스트를 만들어 두거나...새로고침할 때 마다 random으로 내용을 바뀌게끔) .
+//더미텍스트 상태에서 추가해도 목록에 추가
+//+q버튼 클릭 시 색상 변화는 넣어주자
