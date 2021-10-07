f28d7b7c6e741590945099cdc30f1be8cd0e0325
yodiodine <76445841+yodiodine@users.noreply.github.com>
2021-10-07 10:51:27 +0900

    update js
    placeholder의 더미텍스트  추가 / 아무것도 입력하지 않은 상태에서 추가 시 더미텍스트가 할 일로 들어감 / 할 일 입력 후 input 값 리셋

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ window.onload=function(){
     let isSun = true;
     //할 일 목록 배열
     let toDoArray = [];
+    //더미텍스트 배열
+    const dummyText = ["독서30분","헬스 2시간 다녀오기","자격증 공부하기","강아지 산책 1시간","영화 한 편 감상하기",
+    "인터넷 강의 하나 듣기","마트에서 식료품 장보기","토익 모의고사 하나 풀기","짧은 글 필사","영어 회화 30분",
+    "일러스트 한 장 그리기","크로키 30분","게임은 1시간만","스터디 참여하기","쓰레기 버리는 날",
+    "빨래 돌리기","청소기 돌리기","다이소에서 물건 사기","일본어 공부 1시간","고객센터에 문의 전화 넣기"];
+
+
 
     //할 일 체크하기
     function clearOrNotTodo(){
@@ -63,6 +70,7 @@ window.onload=function(){
             })
         })
     }
+
     //========================화면 모드========================//
     //밝기 모드 전환
     bar_btns.forEach((btn)=>{
@@ -124,7 +132,10 @@ window.onload=function(){
             li.childNodes[0].style.borderColor = "#aaa";
         })
     }
+    //===========================================================//
 
+
+    //========================처음 화면 배치========================//
     //localStorage조회하여 배열에 넣고 화면에 보이기
     for(let i=0;i<localStorage.length;i++){
         toDoArray.push({todo : JSON.parse(localStorage.getItem(i)).todo ,clear :  JSON.parse(localStorage.getItem(i)).clear});
@@ -138,7 +149,7 @@ window.onload=function(){
     }
 
     //완료한 할 일 체크박스
-    //배열마다 순회한다 -> 클리어가 true인 배열의 인덱스를 찾아 해당 li의 클래스에 clear를 붙인다.
+    //배열마다 클리어가 true인 배열의 인덱스를 찾아 해당 li의 클래스에 clear를 붙인다.
     toDoArray.forEach((val,idx)=>{
         if(val.clear){
             list.children[idx].setAttribute("class","clear");
@@ -147,7 +158,16 @@ window.onload=function(){
         }
     })
 
-    //========================입력창에 할 일 입력========================//
+    function placeholderRenuwal(){
+        let dummyNum = Math.floor(Math.random() * 19);
+        text.setAttribute("placeholder",dummyText[dummyNum]);
+    }
+    placeholderRenuwal();
+
+    //===========================================================//
+
+
+    //========================입력창에 할 일 입력 로직========================//
     //할 일 추가 함수(+버튼 혹은 enter 키(keyCode : 13))
     add_btn.addEventListener("click",checkValue);
     window.addEventListener("keydown",function(e){
@@ -157,7 +177,7 @@ window.onload=function(){
     });
     //할 일 입력 값 확인 함수
     function checkValue(){
-        !text.value ? alert("할 일을 입력해주세요") : checkTodo(text.value);
+        !text.value ? checkTodo(text.getAttribute("placeholder")) : checkTodo(text.value);
     }
     //할 일 목록 조회 함수
     function checkTodo(txt){
@@ -166,7 +186,12 @@ window.onload=function(){
             toDoArray[i].todo === txt ? check = true : check=false;
             if(check) break;
         }
-        check ? alert("해당 할 일은 이미 존재합니다!") : insertTodo(txt);
+        if(check){
+            alert("해당 할 일은 이미 존재합니다!");
+            placeholderRenuwal();
+        }else{
+            insertTodo(txt);
+        }
     }
     //localStorage 저장 함수
     function insertTodo(txt){
@@ -176,20 +201,18 @@ window.onload=function(){
         localStorage.setItem(toDoArray.length-1, JSON.stringify(toDoArray[toDoArray.length-1]));
         //할 일을 목록에 표현하기 : li 추가
         list.innerHTML+=`<li><span class='check'><i class='fas fa-check'></i></span>${txt}<span class='delete'><i class='far fa-trash-alt'></i></span></li>`;
-        
+        //input초기화
+        text.value = "";
+        placeholderRenuwal();
+        //innerHTML 이후 변수 재할당
         check_btns = document.querySelectorAll(".check");
         delete_btns = document.querySelectorAll(".delete");
+        cleard_list = document.querySelectorAll(".clear");
         clearOrNotTodo();
         deleteTodo();
-        cleard_list = document.querySelectorAll(".clear");
         isSun ? brightCheck() : darkCheck();
     }
+    //===========================================================//
+
+
 }
-
-
-
-//value 추가 후 입력 되어있던 인풋 값 지우기
-//인풋 내 텍스트 내용 바꾸기(예시 내용을 넣거나 ex: 운동 30분 하기)
-//placeholder가 새로고침하거나 등등 바뀜(더미텍스트를 만들어 두거나...새로고침할 때 마다 random으로 내용을 바뀌게끔) .
-//더미텍스트 상태에서 추가해도 목록에 추가
-//+q버튼 클릭 시 색상 변화는 넣어주자
